a1b0a14587b48f8d8ffe0a113d6e774bbad33a9a
Sylletka <sylletka@gmail.com>
2016-09-09 20:36:39 +0200

    modified:   config/web.php

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'jagwMZ5GQ6PhrxvvVfB9ufAALB4TFokE',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
